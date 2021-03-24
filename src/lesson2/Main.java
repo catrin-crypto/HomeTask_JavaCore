5e8e89573225076865a6b15aa3ad160b92fd31a0
Catrin <rude_box@mail.ru>
2021-03-25 00:19:58 +0300

    4 tasks for lesson2 done

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -8,10 +8,45 @@ public class Main {
                 if (array[i] == 0){
                     array[i] = 1;
                 }else array[i] = 0;
-//                System.out.println(array[i]);
+                System.out.println(array[i]);
             }
         }
-
+//        doCalc();
+//        fillArray();
+//        fillDiagonals();
     }
+//
+    public static void fillArray() {
+        int[] emptyArray = new int[8];
 
+            for(int i = 0; i < emptyArray.length; i++) {
+                emptyArray[i] = i * 3;
+                System.out.println(emptyArray[i]);
+            }
+        }
+public static void doCalc(){
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < 6){
+                array[i] *= 2;
+                System.out.println(array[i]);
+            }
+        }
+}
+public static void fillDiagonals(){
+        int n = 6;
+        int[][] doubleArray = new int[n][n];
+        if (doubleArray.length > 0){
+        for (int i = 0; i < doubleArray.length; i++){
+            if(doubleArray[i].length > 0){
+                doubleArray[i][i] = 1;
+                doubleArray[i][n - 1 - i] = 1;
+                for (int j = 0; j < doubleArray[i].length; j++){
+                System.out.print(doubleArray[i][j] + ", ");
+                }
+                System.out.println();
+            }
+        }
+        }
+}
 }
