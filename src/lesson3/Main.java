8b39a4167b4d972925518fb778028f68555bc0cd
Catrin <rude_box@mail.ru>
2021-03-28 23:59:23 +0300

    Game "Guess the number"

---
+++ src/lesson3/Main.java
@@ -0,0 +1,46 @@
+package lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int userDecision;
+
+    do
+    {
+
+        int guessingNumber = random.nextInt(10);
+
+        int maxTryCount = 3;
+
+        for (int tryCount = maxTryCount; tryCount > 0; tryCount--)
+        {
+            System.out.println("Угадайте число от 0 до 9");
+            int userAnswer = scanner.nextInt();
+            if(userAnswer == guessingNumber)
+            {
+                System.out.println("Вы угадали!");
+                break;
+            }else if(userAnswer > guessingNumber)
+            {
+                System.out.println("Загаданное число меньше");
+            }else
+            {
+                System.out.println("Загаданное число больше");
+            }
+            if(tryCount == 1)
+            {
+                System.out.println("Попытки закончились. Вы проиграли!");
+            }
+        }
+        System.out.println("Хотите сыграть ещё раз? Если да, то введите 1, если нет, то 0");
+        userDecision = scanner.nextInt();
+    } while(userDecision == 1);
+
+    scanner.close();
+    }
+}
